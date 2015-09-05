3b026d2ffc37a748663fb710bc6664bc3c11136e
Akash Subramanian <akashsubramanian@Akashs-MacBook-Pro.local>
2015-09-04 20:34:42 -0400

    added windows framework file

---
+++ craveit/WindowsAzureMobileServices.framework/Versions/A/Headers/WindowsAzureMobileServices.h
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+#ifndef WindowsAzureMobileServices_WindowsAzureMobileServices_h
+#define WindowsAzureMobileServices_WindowsAzureMobileServices_h
+
+#import "MSClient.h"
+#import "MSCoreDataStore.h"
+#import "MSDateOffset.h"
+#import "MSError.h"
+#import "MSFilter.h"
+#import "MSLoginController.h"
+#import "MSPullSettings.h"
+#import "MSPush.h"
+#import "MSQuery.h"
+#import "MSQueryResult.h"
+#import "MSSyncContext.h"
+#import "MSSyncContextReadResult.h"
+#import "MSSyncTable.h"
+#import "MSTable.h"
+#import "MSTableOperation.h"
+#import "MSTableOperationError.h"
+#import "MSUser.h"
+
+#define WindowsAzureMobileServicesSdkMajorVersion 2
+#define WindowsAzureMobileServicesSdkMinorVersion 2
+#define WindowsAzureMobileServicesSdkBuildVersion 30818
+
+#endif
