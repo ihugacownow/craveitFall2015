975dcd2dbb17c304a33218e07919eb9a09943b5b
Akash Subramanian <akashsubramanian@levyvlan502.1874.wlan.wireless-resnet.upenn.edu>
2015-09-05 22:37:42 -0400

    pushing to access distance api:

--- craveit/MapTasks.swift
+++ craveit/MapTasks.swift
@@ -138,26 +138,97 @@ class MapTasks: NSObject
     }
     
     
-    func getDirections(origin: CLLocationCoordinate2D!, destination: String!, waypoints: Array<String>!, travelMode: AnyObject!, completionHandler: ((status: String, success: Bool) -> Void)) {
-        
-       // var legs = [Dictionary<NSObject, AnyObject>]()
-        
+//    func getDirections(origin: CLLocationCoordinate2D!, destination: String!, waypoints: Array<String>!, travelMode: AnyObject!, completionHandler: ((status: String, success: Bool) -> Void)) {
+//        
+//       // var legs = [Dictionary<NSObject, AnyObject>]()
+//        
+//        if let originLocation = origin {
+//            if let destinationLocation = destination {
+//               // var directionsURLString = baseURLDirections + "origin=" + originLocation + "&destination=" + destinationLocation + "&units=imperial&alternatives=true&sensor=false"
+//                var directionsURLString = baseURLDirections + "origin=" + "\(origin.latitude)" + "," + "\(origin.longitude)" + "&destination=" + destinationLocation + "&units=imperial&alternatives=true&sensor=false"
+//                
+//                directionsURLString = directionsURLString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+//                
+//                let directionsURL = NSURL(string: directionsURLString)
+//                
+//                
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    let directionsData = NSData(contentsOfURL: directionsURL!)
+//                    
+//                    let error: NSError? = nil
+//                    let dictionary: Dictionary<NSObject, AnyObject> = NSJSONSerialization.JSONObjectWithData(directionsData!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! Dictionary<NSObject, AnyObject>
+//
+//                    
+//                    if (error != nil) {
+//                        println(error)
+//                        completionHandler(status: "", success: false)
+//                    }
+//                    else {
+//                        let status = dictionary["status"] as! String
+//                        
+//                        if status == "OK" {
+//                            
+//                            for route in dictionary["routes"] as! Array<Dictionary<NSObject, AnyObject>> {
+//                                
+//                                self.possibleRoutes.append(route)
+//                                self.overviewPolylineForRoute.append(route["overview_polyline"] as! Dictionary<NSObject, AnyObject>)
+//                                let legs = route["legs"] as! Array<Dictionary<NSObject, AnyObject>>
+//                                let startLocationDictionary = legs[0]["start_location"] as! Dictionary<NSObject, AnyObject>
+//                                
+//                                    self.originCoordinate = CLLocationCoordinate2DMake(startLocationDictionary["lat"] as! Double, startLocationDictionary["lng"] as! Double)
+//                                
+//                                let endLocationDictionary = legs[legs.count - 1]["end_location"] as! Dictionary<NSObject, AnyObject>
+//                                self.destinationCoordinate = CLLocationCoordinate2DMake(endLocationDictionary["lat"] as! Double, endLocationDictionary["lng"] as! Double)
+//                                
+//                                self.originAddress = legs[0]["start_address"] as! String
+//                                self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
+//                                
+//                                //self.calculateTotalDistanceAndDuration()
+//                                
+//                            }
+//                            
+//                            
+//        
+//                            
+//                            
+//                            
+//                           
+//                            
+//                            completionHandler(status: status, success: true)
+//                        }
+//                        else {
+//                            completionHandler(status: status, success: false)
+//                             
+//                        }
+//                    }
+//                })
+//            }
+//            else {
+//                completionHandler(status: "Destination is nil.", success: false)
+//            }
+//        }
+//        else {
+//            completionHandler(status: "Origin is nil", success: false)
+//        }
+//    }
+    
+    
+       func getDirections(origin: CLLocationCoordinate2D!, destination: String!, waypoints: Array<String>!, travelMode: AnyObject!, completionHandler: ((status: String, success: Bool) -> Void)) {
         if let originLocation = origin {
             if let destinationLocation = destination {
-               // var directionsURLString = baseURLDirections + "origin=" + originLocation + "&destination=" + destinationLocation + "&units=imperial&alternatives=true&sensor=false"
                 var directionsURLString = baseURLDirections + "origin=" + "\(origin.latitude)" + "," + "\(origin.longitude)" + "&destination=" + destinationLocation + "&units=imperial&alternatives=true&sensor=false"
+
+
                 
                 directionsURLString = directionsURLString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
                 
                 let directionsURL = NSURL(string: directionsURLString)
                 
-                
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     let directionsData = NSData(contentsOfURL: directionsURL!)
                     
-                    let error: NSError? = nil
-                    let dictionary: Dictionary<NSObject, AnyObject> = NSJSONSerialization.JSONObjectWithData(directionsData!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! Dictionary<NSObject, AnyObject>
-
+                    var error: NSError?
+                    let dictionary: Dictionary<NSObject, AnyObject> = NSJSONSerialization.JSONObjectWithData(directionsData!, options: NSJSONReadingOptions.MutableContainers, error: &error) as! Dictionary<NSObject, AnyObject>
                     
                     if (error != nil) {
                         println(error)
@@ -167,38 +238,26 @@ class MapTasks: NSObject
                         let status = dictionary["status"] as! String
                         
                         if status == "OK" {
+                            self.selectedRoute = (dictionary["routes"] as! Array<Dictionary<NSObject, AnyObject>>)[0]
+                            self.overviewPolyline = self.selectedRoute["overview_polyline"] as! Dictionary<NSObject, AnyObject>
                             
-                            for route in dictionary["routes"] as! Array<Dictionary<NSObject, AnyObject>> {
-                                
-                                self.possibleRoutes.append(route)
-                                self.overviewPolylineForRoute.append(route["overview_polyline"] as! Dictionary<NSObject, AnyObject>)
-                                let legs = route["legs"] as! Array<Dictionary<NSObject, AnyObject>>
-                                let startLocationDictionary = legs[0]["start_location"] as! Dictionary<NSObject, AnyObject>
-                                
-                                    self.originCoordinate = CLLocationCoordinate2DMake(startLocationDictionary["lat"] as! Double, startLocationDictionary["lng"] as! Double)
-                                
-                                let endLocationDictionary = legs[legs.count - 1]["end_location"] as! Dictionary<NSObject, AnyObject>
-                                self.destinationCoordinate = CLLocationCoordinate2DMake(endLocationDictionary["lat"] as! Double, endLocationDictionary["lng"] as! Double)
-                                
-                                self.originAddress = legs[0]["start_address"] as! String
-                                self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
-                                
-                                //self.calculateTotalDistanceAndDuration()
-                                
-                            }
+                            let legs = self.selectedRoute["legs"] as! Array<Dictionary<NSObject, AnyObject>>
                             
+                            let startLocationDictionary = legs[0]["start_location"] as! Dictionary<NSObject, AnyObject>
+                            self.originCoordinate = CLLocationCoordinate2DMake(startLocationDictionary["lat"] as! Double, startLocationDictionary["lng"] as! Double)
                             
-        
+                            let endLocationDictionary = legs[legs.count - 1]["end_location"] as! Dictionary<NSObject, AnyObject>
+                            self.destinationCoordinate = CLLocationCoordinate2DMake(endLocationDictionary["lat"] as! Double, endLocationDictionary["lng"] as! Double)
                             
+                            self.originAddress = legs[0]["start_address"] as! String
+                            self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
                             
-                            
-                           
+                            self.calculateTotalDistanceAndDuration()                        
                             
                             completionHandler(status: status, success: true)
                         }
                         else {
                             completionHandler(status: status, success: false)
-                             
                         }
                     }
                 })
